8c1e6cbcc166d7ee19384d86d2956d053feae218
Bayardo Enrique Ortiz Beltre <20220016@itla.edu.do>
2024-08-01 15:00:15 -0400

    V0.9.9.1

---
+++ Library.API/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Library.Infrastructure.Datos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        private readonly ContextoBiblioteca _context;
+
+        public TestController(ContextoBiblioteca context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("test-connection")]
+        public async Task<IActionResult> TestConnection()
+        {
+            try
+            {
+                var canConnect = await _context.Database.CanConnectAsync();
+                if (canConnect)
+                {
+                    return Ok("Connection successful");
+                }
+                else
+                {
+                    return StatusCode(500, "Connection failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Connection failed: {ex.Message}");
+            }
+        }
+    }
+
+
+}
